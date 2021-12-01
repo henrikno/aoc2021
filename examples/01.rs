eb48737a5f935a5ae8f45a1514ed160b11ad63fe
Henrik Nordvik <henrikno@gmail.com>
2021-12-01 12:15:08 -0800

    Day 01: Use tuple_window and iterators, avoid collect

--- examples/01.rs
+++ examples/01.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use std::error::Error;
+use itertools::Itertools;
 
 fn main() -> Result<(), Box<dyn Error>> {
     assert_eq!(part1("data/01.example")?, 7);
@@ -20,17 +21,18 @@ fn part1(filename: &str) -> Result<i32, Box<dyn Error>> {
 fn part2(filename: &str) -> Result<i32, Box<dyn Error>> {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
-    let ints = file_to_ints(reader);
-    return Ok(increments(ints.windows(3).map(|x|x.iter().sum()).collect()));
+    let ints = file_to_ints::<i32>(reader);
+    return Ok(increments(ints.into_iter().tuple_windows().map(|(x, y, z)| x + y + z)));
 }
 
-fn increments(ints: Vec<i32>) -> i32 {
-    ints.windows(2).filter(|val| val[0] < val[1]).count() as i32
+fn increments<I>(ints: I) -> i32
+where I: IntoIterator<Item = i32>,
+{
+    ints.into_iter().tuple_windows().filter(|(a, b)| a < b).count() as i32
 }
 
-fn file_to_ints(reader: BufReader<File>) -> Vec<i32> {
+fn file_to_ints<T: std::str::FromStr>(reader: BufReader<File>) -> impl Iterator<Item=T> {
     reader.lines()
-        .flat_map(|x| x.map(|y| y.parse::<i32>()))
+        .flat_map(|x| x.map(|y| y.parse::<T>()))
         .flatten()
-        .collect()
 }
